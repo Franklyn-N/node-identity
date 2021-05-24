52fc119f3968c0732833b647cbe7ebdd6db72c08
Franklyn-N <godfather.franklyn@gmail.com>
2021-05-24 09:01:22 +0100

    deploy

--- app.js
+++ app.js
@@ -57,6 +57,14 @@ app.use((req, res, next) => {
 app.use('/data', dataRoutes);
 app.use("/auth", authRoutes);
 
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
